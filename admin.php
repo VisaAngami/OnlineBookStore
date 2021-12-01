1a57403ce2ef56940ed9efa108ce6eb980124b3d
Visakhotuo Angami <91118691+VisaAngami@users.noreply.github.com>
2021-12-01 19:18:39 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -5,13 +5,13 @@
 
 	<form class="form-horizontal" method="post" action="admin_verify.php">
 		<div class="form-group">
-			<label for="name" class="control-label col-md-4">Name</label>
+			<label for="name" class="control-label col-md-4">UserName</label>
 			<div class="col-md-4">
 				<input type="text" name="name" class="form-control">
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="pass" class="control-label col-md-4">Pass</label>
+			<label for="pass" class="control-label col-md-4">Password</label>
 			<div class="col-md-4">
 				<input type="password" name="pass" class="form-control">
 			</div>
